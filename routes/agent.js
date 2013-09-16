9d8944aa6de5605074bac8e86bfe0c35525843de
sven4ask <sstam@ask-cs.com>
2013-09-16 17:44:11 +0200

    add medium to id of bid

--- routes/agent.js
+++ routes/agent.js
@@ -1,6 +1,6 @@
 var AskFast = require("askfast"),
     url = require('url'),
-    host = null;
+    host = null,
     path = "agent";
 
 var Agent = function(bid, req, res, next) {
@@ -12,20 +12,23 @@ var Agent = function(bid, req, res, next) {
 
     var responder = query.responder;
     var prefMedium = query.preferred_medium;
+    var medium = query.medium;
+    if(medium ==null && prefMedium=="audio/wav")
+        medium="phone";
     if(query.function=="name") {
         return askfast.getResult().receive(function(answer) {
             var name = "test";
             if(answer!=null && answer.answer_text!=null) {
                 name = answer.answer_text;
-                return bid.create(responder, name, null, function(err, result) {
+                return bid.create(responder, medium, name, null, function(err, result) {
 
-                    askfast.ask(answer.answer_text+" geef een aantal op dat u denkt dat in de vaas zit?",path+"?function=bid&responder="+responder);
+                    askfast.ask(answer.answer_text+" geef een aantal op dat u denkt dat in de vaas zit?",path+"?function=bid&responder="+responder+"&medium="+medium);
 
                     res.writeHead(200, {'Content-Type': 'application/json'});
                     res.end(askfast.finalize());
                 });
             } else {
-                askfast.ask("Wat leuk dat u mee doet met het raadspel. Wat is uw naam?",path+"?function=name&responder="+responder);
+                askfast.ask("Wat leuk dat u mee doet met het raadspel. Wat is uw naam?",path+"?function=name&responder="+responder+"&medium="+medium);
             }
             res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(askfast.finalize());
@@ -37,7 +40,7 @@ var Agent = function(bid, req, res, next) {
                 if(answer!=null && answer.answer_text!=null) {
                     if(isNaN(answer.answer_text) || parseInt(answer.answer_text)<=0) {
                         console.log("Amount NaN?", answer.answer_text);
-                        askfast.ask("/audio/invalid.wav",path+"?function=bid&responder="+responder+"&preferred_medium=audio/wav");
+                        askfast.ask("/audio/invalid.wav",path+"?function=bid&responder="+responder+"&medium="+medium+"&preferred_medium=audio/wav");
                         res.writeHead(200, {'Content-Type': 'application/json'});
                         res.end(askfast.finalize());
                         return;
@@ -45,17 +48,17 @@ var Agent = function(bid, req, res, next) {
                         amount = parseInt(answer.answer_text);
                     }
                 } else {
-                    askfast.ask("/audio/invalid.wav",path+"?function=bid&responder="+responder+"&preferred_medium=audio/wav");
+                    askfast.ask("/audio/invalid.wav",path+"?function=bid&responder="+responder+"&medium="+medium+"&preferred_medium=audio/wav");
                     res.writeHead(200, {'Content-Type': 'application/json'});
                     res.end(askfast.finalize());
                     return;
                 }
-                return bid.update(responder, amount, function(err, result) {
+                return bid.update(responder, medium, amount, function(err, result) {
 
                     if(!err) {
                         askfast.say("/audio/bedankt.wav");
                     } else {
-                        askfast.ask("/audio/start.wav",path+"?function=bid&responder="+responder+"&preferred_medium=audio/wav");
+                        askfast.ask("/audio/start.wav",path+"?function=bid&responder="+responder+"&medium="+medium+"&preferred_medium=audio/wav");
                     }
                     res.writeHead(200, {'Content-Type': 'application/json'});
                     res.end(askfast.finalize());
@@ -64,7 +67,7 @@ var Agent = function(bid, req, res, next) {
                 if(answer!=null && answer.answer_text!=null) {
                     if(isNaN(answer.answer_text) || parseInt(answer.answer_text)<=0) {
                         console.log("Amount NaN?", answer.answer_text);
-                        askfast.ask("U heeft een ongeldig aantal gegeven. Geef een getal tussen 1 en 100 000.",path+"?function=bid&responder="+responder);
+                        askfast.ask("U heeft een ongeldig aantal gegeven. Geef een getal tussen 1 en 100 000.",path+"?function=bid&responder="+responder+"&medium="+medium);
                         res.writeHead(200, {'Content-Type': 'application/json'});
                         res.end(askfast.finalize());
                         return;
@@ -72,17 +75,17 @@ var Agent = function(bid, req, res, next) {
                         amount = parseInt(answer.answer_text);
                     }
                 } else {
-                    askfast.ask("U heeft een ongeldig aantal gegeven. Geef een getal tussen 1 en 100 000.",path+"?function=bid&responder="+responder);
+                    askfast.ask("U heeft een ongeldig aantal gegeven. Geef een getal tussen 1 en 100 000.",path+"?function=bid&responder="+responder+"&medium="+medium);
                     res.writeHead(200, {'Content-Type': 'application/json'});
                     res.end(askfast.finalize());
                     return;
                 }
-                return bid.update(responder, amount, function(err, result) {
+                return bid.update(responder, medium, amount, function(err, result) {
 
                     if(!err) {
                         askfast.say("Bedankt voor uw gok!");
                     } else {
-                        askfast.ask("Er is iets misgegaan geef nogmaals het aantal op dat u denkt dat in de vaas zit?",path+"?function=bid&responder="+responder);
+                        askfast.ask("Er is iets misgegaan geef nogmaals het aantal op dat u denkt dat in de vaas zit?",path+"?function=bid&responder="+responder+"&medium="+medium);
                     }
                     res.writeHead(200, {'Content-Type': 'application/json'});
                     res.end(askfast.finalize());
@@ -90,11 +93,11 @@ var Agent = function(bid, req, res, next) {
             }
         });
     } else {
-        return bid.findByAddress(responder, function(err, doc){
+        return bid.findByAddress(medium+"_"+responder, function(err, doc){
             if(prefMedium!=null && prefMedium=="audio/wav") {
                 if(err && err.error=='not_found') {
-                    return bid.create(responder, responder.substring(0,responder.length-2)+'**', null, function(err, result) {
-                        askfast.ask("audio/start.wav",path+"?function=bid&responder="+responder+"&preferred_medium=audio/wav");
+                    return bid.create(responder, medium, responder.substring(0,responder.length-2)+'**', null, function(err, result) {
+                        askfast.ask("audio/start.wav",path+"?function=bid&responder="+responder+"&medium="+medium+"&preferred_medium=audio/wav");
                         res.writeHead(200, {'Content-Type': 'application/json'});
                         res.end(askfast.finalize());
                     });
@@ -102,17 +105,17 @@ var Agent = function(bid, req, res, next) {
                     if(doc.amount) {
                         askfast.say("/audio/retry.wav");
                     } else {
-                        askfast.ask("audio/start.wav",path+"?function=bid&responder="+responder+"&preferred_medium=audio/wav");
+                        askfast.ask("audio/start.wav",path+"?function=bid&responder="+responder+"&medium="+medium+"&preferred_medium=audio/wav");
                     }
                 }
             } else {
                 if(err && err.error=='not_found') {
-                    askfast.ask("Wat leuk dat u mee doet met het raadspel. Wat is uw naam?",path+"?function=name&responder="+responder);
+                    askfast.ask("Wat leuk dat u mee doet met het raadspel. Wat is uw naam?",path+"?function=name&responder="+responder+"&medium="+medium);
                 } else {
                     if(doc.amount) {
                         askfast.say("U heeft al reeds een bod geplaatst?");
                     } else {
-                        askfast.ask("Wat leuk dat u mee doet met het raadspel. Wat is uw naam?",path+"?function=name&responder="+responder);
+                        askfast.ask("Wat leuk dat u mee doet met het raadspel. Wat is uw naam?",path+"?function=name&responder="+responder+"&medium="+medium+"&medium="+medium);
                     }
                 }
             }
